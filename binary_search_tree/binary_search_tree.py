ee776da0df85251cf9480c6a40b4a3a86cb7dac3
allisonkydy <allisnelly@gmail.com>
2020-01-29 15:00:19 -0600

    binary search tree contains complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -34,7 +34,23 @@ class BinarySearchTree:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        node = self
+        while True:
+            # if values match, return true
+            if target == node.value:
+                return True
+            # if target is less, move left
+            elif target < node.value:
+                # if no left child, tree does not contain target
+                if node.left is None:
+                    return False
+                node = node.left
+            # if target is greater or equal, move right
+            else:
+                # if no right child, tree does not contain target
+                if node.right is None:
+                    return False
+                node = node.right
 
     # Return the maximum value found in the tree
     def get_max(self):
