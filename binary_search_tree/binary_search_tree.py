067277e65299362064b686344b32f1614b9c9195
allisonkydy <allisnelly@gmail.com>
2020-01-30 14:59:00 -0600

    ok in_order_print is fixed now

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -149,9 +149,9 @@ class BinarySearchTree:
     def in_order_print(self, node):
         if self.left:
             self.left.in_order_print(self.left)
+        print(self.value)
         if self.right:
             self.right.in_order_print(self.right)
-        print(self.value)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
